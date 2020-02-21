7873638e63d29fb2d6acc7cb104b70d5451eae62
RadenAR <radenabdulrahman@gmail.com>
2020-02-21 10:38:26 -0500

    Added production URL
    added the link to the heroku backend to the config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://raden-restaurant-app.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
