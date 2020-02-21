7025d09c8ee6f2ba19542e3f573aae1e0512fd42
RadenAR <radenabdulrahman@gmail.com>
2020-02-21 12:20:23 -0500

    Added authentication to app
    Added event handlers to app.js
    Added events to events.js
    Added api calls to api.js
    Added forms for index.html
    Added styles (not working yet) to scss file

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const store = require('../store')
+
+const onSignUpSuccess = response => {
+  $('#message').text(`${response.user.email} successfully signed up`)
+  $('#sign-up').trigger('reset')
+
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+}
+const onSignUpFailure = response => {
+  $('#message').text('Sign up failed')
+  $('#sign-up').trigger('reset')
+
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+}
+
+const onSignInSuccess = response => {
+  $('#message').text(`${response.user.email} successfully signed in!`)
+  $('#sign-in').trigger('reset')
+  store.user = response.user
+
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+}
+
+const onSignInFailure = response => {
+  $('#message').text('Sign in failed')
+  $('#sign-in').trigger('reset')
+
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+}
+
+const onChangeSuccess = () => {
+  $('#message').text(`Successfully changed password`)
+  $('#change-pw').trigger('reset')
+
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+}
+
+const onChangeFailure = () => {
+  $('#message').text('Change password failed')
+  $('#change-pw').trigger('reset')
+
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+}
+
+const onSignOutSuccess = () => {
+  $('#message').text(`Successfully Signed Out`)
+  $('#change-pw').trigger('reset')
+  store.user = null
+
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+}
+
+const onSignOutFailure = () => {
+  $('#message').text('Sign Out failed')
+
+  $('#message').removeClass('success')
+  $('#message').addClass('failure')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangeSuccess,
+  onChangeFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
